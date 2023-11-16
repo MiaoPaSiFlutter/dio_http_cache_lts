ce1ac00318b9ce4e5ae334a9a4364eb6e3142f2e
zhuyuhui <zhuyuhui@odianyun.com>
2023-11-16 10:17:19 +0800

    update dio: ^5.3.2

--- .ios/Flutter/flutter_export_environment.sh
+++ .ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/zhuyuhui/fvm/versions/3.13.9"
-export "FLUTTER_APPLICATION_PATH=/Users/zhuyuhui/Documents/Github/MiaoPaSiFlutter/dio_http_cache_lts/example"
+export "FLUTTER_APPLICATION_PATH=/Users/zhuyuhui/Documents/Github/MiaoPaSiFlutter/dio_http_cache_lts"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NAME=0.4.1"
+export "FLUTTER_BUILD_NUMBER=0.4.1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
